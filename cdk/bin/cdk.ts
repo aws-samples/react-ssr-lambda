19097fc8e4bac917da57b81d13b9b23dd30af1be
Eric Halsey <ehalseyhb@gmail.com>
2022-11-21 09:31:48 -0800

    cd  command not working

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -13,26 +13,26 @@ const demoEnv = { region: "us-east-1" };
 const app = new cdk.App();
 
 export class MyPipelineStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-      super(scope, id, props);
-  
-      const pipeline = new CodePipeline(this, 'Pipeline', {
-        pipelineName: 'MyPipeline',
-        synth: new CodeBuildStep("SynthStep", {
-          input: CodePipelineSource.connection(
-            "ehalsey/react-ssr-lambda",
-            "main",
-            {
-              connectionArn:
-                "arn:aws:codestar-connections:us-west-1:263870947518:connection/7d190900-3cfe-4f09-b4ad-60b1ad7a1c0d"
-            }
-          ),
-          installCommands: ["yarn install"],
-          commands: ["cd ./cdk","yarn run build", "yarn cdk synth"]
-        })
-      });
-    }
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new CodeBuildStep("SynthStep", {
+        input: CodePipelineSource.connection(
+          "ehalsey/react-ssr-lambda",
+          "main",
+          {
+            connectionArn:
+              "arn:aws:codestar-connections:us-west-1:263870947518:connection/7d190900-3cfe-4f09-b4ad-60b1ad7a1c0d"
+          }
+        ),
+        installCommands: ["yarn install"],
+        commands: ["echo here1", "cd ./cdk", "yarn run build", "yarn cdk synth"]
+      })
+    });
   }
+}
 
 
 new MyPipelineStack(app, 'MyPipelineStack', {
